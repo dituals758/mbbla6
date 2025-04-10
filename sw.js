51d34be25b1e8f4951e38f60e39ee584ec33ad73
dituals758 <dituals758@gmail.com>
2025-04-10 21:49:12 +0300

    update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', e => {
   e.waitUntil(
       caches.open('finance').then(cache => {
-          return cache.addAll(['/', '/index.html']);
+          return cache.addAll(['', 'index.html']);
       })
   );
 });
